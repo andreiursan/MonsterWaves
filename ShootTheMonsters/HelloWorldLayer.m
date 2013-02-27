777a2d2a43c5a0bfe37d226a742bc2bfb15fa867
Andrei Ursan <hello@andreiursan.com>
2013-02-27 23:40:20 +0100

    Added Monsters

--- ShootTheMonsters/HelloWorldLayer.m
+++ ShootTheMonsters/HelloWorldLayer.m
@@ -29,13 +29,41 @@
 {
 	if( (self=[super initWithColor:ccc4(255,255,255,255)]) ) {
     player = [CCSprite spriteWithFile:@"player.png"];
-    CGSize winSize = [[CCDirector sharedDirector] winSize];
-    player.position = ccp(player.contentSize.width/2, winSize.height/2);
+    player.position = ccp(player.contentSize.width/2, [self windowSize].height/2);
     [self addChild: player];
+    [self schedule:@selector(gameLogic:) interval:1.0];
 	}
 	return self;
 }
 
+- (void) gameLogic:(ccTime)dt{
+  [self addMonster];
+}
+
+- (void) addMonster{
+  // Add Monster
+  CCSprite *monster = [CCSprite spriteWithFile:@"monster.png"];
+  int minY = monster.contentSize.height / 2;
+  int maxY = [self windowSize].height - minY;
+  int actualY = (arc4random() % (maxY-minY)) + minY;
+  monster.position = ccp([self windowSize].width, actualY);
+  [self addChild:monster];
+  
+  // Determine Mosnter's speed
+  int minDuration = 2.0;
+  int maxDuration = 4.0;
+  int actualDuration = (arc4random() % (maxDuration - minDuration)) + minDuration;
+  CCMoveTo *moveAction = [CCMoveTo actionWithDuration:actualDuration position:ccp(-monster.contentSize.width/2, actualY)];
+  CCCallBlockN *moveActionDone = [CCCallBlockN actionWithBlock:^(CCNode *node){
+    [node removeFromParentAndCleanup:YES];
+  }];
+  
+  [monster runAction:[CCSequence actions:moveAction, moveActionDone, nil]];
+}
+
+- (CGSize) windowSize{
+  return [[CCDirector sharedDirector] winSize];
+}
 
 - (void) dealloc
 {
