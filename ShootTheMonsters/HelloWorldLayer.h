d75b70ae77204a761e9d1e573e95c7460c7a58a5
Andrei Ursan <hello@andreiursan.com>
2013-02-27 22:38:07 +0100

    Added Assets, and player to Helloworld Scene

--- ShootTheMonsters/HelloWorldLayer.h
+++ ShootTheMonsters/HelloWorldLayer.h
@@ -8,16 +8,14 @@
 
 
 #import <GameKit/GameKit.h>
-
-// When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 
-// HelloWorldLayer
-@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+
+@interface HelloWorldLayer : CCLayerColor
 {
+  CCSprite *player;
 }
 
-// returns a CCScene that contains the HelloWorldLayer as the only child
 +(CCScene *) scene;
 
 @end
