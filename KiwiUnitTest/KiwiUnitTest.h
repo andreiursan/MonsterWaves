52b5a79f35add2d67166b16fd092a6f899ae588f
Andrei Ursan <hello@andreiursan.com>
2013-04-15 02:23:55 +0200

    Kiwi 'specs' are part of the project now

---
+++ KiwiUnitTest/KiwiUnitTest.h
@@ -0,0 +1,13 @@
+//
+//  KiwiUnitTest.h
+//  KiwiUnitTest
+//
+//  Created by Andrei Ursan on 4/15/13.
+//  Copyright (c) 2013 Andrei Ursan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KiwiUnitTest : SenTestCase
+
+@end
--- Podfile
+++ Podfile
@@ -1,4 +1,7 @@
 platform :ios
 
 pod 'cocos2d', '~> 2.0'
+target :KiwiUnitTest, :exclusive => true do
+  pod 'Kiwi', '~> 2.0.6'
+end
 
